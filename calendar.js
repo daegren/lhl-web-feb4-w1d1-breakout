6f56fdd5208f6f6f1f1ad1805907eab110cdbdc2
David Mills <dave@1morepx.com>
2019-02-04 16:24:43 -0500

    full solution
    adding calculation of suffix based on day number

--- calendar.js
+++ calendar.js
@@ -15,14 +15,37 @@ var monthStrings = [
 
 function talkingCalendar(input) {
   var parts = input.split('/');
-  console.log(parts);
 
   var year = parseInt(parts[0], 10);
   var month = parseInt(parts[1], 10);
   var day = parseInt(parts[2], 10);
 
   var monthStr = monthStrings[month - 1];
-  console.log('monthStr', monthStr);
+  var daySuffix;
+  switch(day) {
+    case 1:
+    case 21:
+    case 31:
+      daySuffix = 'st';
+    break;
+
+    case 2:
+    case 22:
+      daySuffix = 'nd';
+    break;
+
+    case 3:
+    case 23:
+      daySuffix = 'rd';
+    break;
+
+    default:
+      daySuffix = 'th';
+    break;
+  }
+
+  return `${monthStr} ${day}${daySuffix}, ${year}`;
+  // return monthStr + " " + day + daySuffix + ", " + year;
 }
 
 console.log(talkingCalendar("2017/12/02"));
